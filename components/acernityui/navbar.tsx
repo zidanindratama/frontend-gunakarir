2177c0f0b2fc0c4b566e0a41e08488435103064b
zidanindratama <zidanindratama31@gmail.com>
2025-05-16 08:43:21 +0700

    fix: select in dialog

--- components/acernityui/navbar.tsx
+++ components/acernityui/navbar.tsx
@@ -70,7 +70,7 @@ export const Navbar = ({ children, className }: NavbarProps) => {
     <motion.div
       ref={ref}
       // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
-      className={cn("fixed inset-x-0 top-10 z-40 w-full", className)}
+      className={cn("sticky inset-x-0 top-20 z-40 w-full", className)}
     >
       {React.Children.map(children, (child) =>
         React.isValidElement(child)
